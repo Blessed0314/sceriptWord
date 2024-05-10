5b1e73a700ed36fa407e7a95d2e8595ca75c48b0
blessed0314 <christian.daniel999@gmail.com>
2024-05-10 11:06:25 -0500

    Se actualiza el script con todas las modificaciones que debe hacer al excel

--- script.py
+++ script.py
@@ -2,89 +2,134 @@ import openpyxl, os, subprocess, pandas as pd
 from docx import Document
 from pathlib import Path
 
+EXCEL_NAME = 'Plantilla.xlsx'
+COLUMN_TITLES = ['Asignatura', 'Nota', 'Status copy']
+COLUMN_WIDTHS = [50, 10, 15]
 
-'''
-Función para obtener almacenar en variables los directorios de las carpetas y para crear el documento donde se guardara el archivo final.
-'''
 def get_folders(targetDir, finalDir, docName):
-  global finalDoc, docDir, targetDocs #Creamos las variables que se usaran en el script en forma global
-  finalDoc = Document() # Creamos el objeto documento donde se almacenara toda la info
-  docDir = finalDir + docName + ".docx" # Directorio donde se guardará el documento final
-  targetDocs = targetDir #Directorio donde están los programas academicos
+  finalDoc = Document()
+  docDir = finalDir + docName + ".docx"
+  targetDocs = targetDir
+  return finalDoc, docDir, targetDocs
 
-#Funcion que crea un excel base para que se copien las asignaturas y las notas de las mismas
-def createTemplate ():
-  template = openpyxl.Workbook()#Creamos el objeto o documento de excel
-  sheet1 = template.active #Cargamos la primera hoja del excel
-  sheet1['A1'] = 'Asignatura' #Colocamos el titulo a la primera celda
-  sheet1.column_dimensions['A'].width = 50
-  sheet1['B1'] = 'Nota' #Colocamos el titulo a la segunda celda
-  sheet1.column_dimensions['B'].width = 10
-  sheet1['C1'] = 'Status copy' #Colocamos el titulo a la tercera celda
-  sheet1.column_dimensions['C'].width = 15
-  excelName = 'Plantilla.xlsx'
-  template.save(excelName)
-  template.close()
-  
-  # Abrir el archivo Excel recién creado
-  # Comprobar si estamos en Windows
+def adjust_column_widths(excel_path):
+    workbook = openpyxl.load_workbook(excel_path)
+    sheet = workbook.active
+
+    for column in sheet.columns:
+        max_length = 0
+        column = [cell for cell in column]
+        for cell in column:
+            try:
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+        adjusted_width = (max_length + 2)
+        sheet.column_dimensions[column[0].column_letter].width = adjusted_width
+
+    workbook.save(excel_path)
+    workbook.close()
+
+def create_template():
+    template = openpyxl.Workbook()
+    sheet = template.active
+    sheet.title = 'Sheet1'
+    
+    for i, title in enumerate(COLUMN_TITLES):
+        cell = chr(65 + i) + '1'
+        sheet[cell] = title
+        sheet.column_dimensions[chr(65 + i)].width = COLUMN_WIDTHS[i]
+
+    template.save(EXCEL_NAME)
+    template.close()
+    
+    open_excel(EXCEL_NAME)
+
+def open_excel(excelName):
   if os.name == 'nt':  
     subprocess.Popen(['start', 'excel', excelName], shell=True)
-  # Comprobar si estamos en sistemas basados en Unix/Linux
   if os.name == 'posix':  
     subprocess.Popen(['xdg-open', excelName])
 
-# Función para copiar párrafos
 def copy_paragraphs(source_paragraphs, target_document):
     for source_paragraph in source_paragraphs:
-        new_paragraph = target_document.add_paragraph()
-        for run in source_paragraph.runs:
-            new_run = new_paragraph.add_run(run.text)
-            new_run.font.size = run.font.size  # Mantener el tamaño de la letra
-            new_run.bold = run.bold
-            new_run.italic = run.italic
-            new_run.underline = run.underline
-            
+        copy_paragraph(source_paragraph, target_document)
 
-# Función para copiar tablas
+def copy_paragraph(source_paragraph, target_document):
+    new_paragraph = target_document.add_paragraph()
+    for run in source_paragraph.runs:
+        copy_run(run, new_paragraph)
+
+def copy_run(run, new_paragraph):
+    new_run = new_paragraph.add_run(run.text)
+    new_run.font.size = run.font.size
+    new_run.bold = run.bold
+    new_run.italic = run.italic
+    new_run.underline = run.underline
+
 def copy_tables(source_tables, target_document):
     for source_table in source_tables:
-        new_table = target_document.add_table(rows=len(source_table.rows), cols=len(source_table.columns))
-        for i, row in enumerate(source_table.rows):
-            for j, cell in enumerate(row.cells):
-                new_table.cell(i, j).text = cell.text
-                new_table.cell(i, j).paragraphs[0].runs[0].font.size = cell.paragraphs[0].runs[0].font.size
+        copy_table(source_table, target_document)
 
-# Consolidación de documentos de Word
-def runScript (targetDir, finalDir, docName, excelDir):
-  get_folders(targetDir, finalDir, docName)
-  root = Path(targetDocs)
-  #plantilla.xlsx es la ruta del excel, se debe reemplazar por la variable
-  df = pd.read_excel(excelDir, sheet_name='Sheet', header = 0)
-  # Se elimina los espacios que se coloquen por error en el contenido de las columnas
-  df = df.map(lambda x: x.strip() if isinstance(x, str) else x)
+def copy_table(source_table, target_document):
+    new_table = target_document.add_table(rows=len(source_table.rows), cols=len(source_table.columns))
+    for i, row in enumerate(source_table.rows):
+        for j, cell in enumerate(row.cells):
+            new_table.cell(i, j).text = cell.text
 
-# Iterar sobre las filas del DataFrame
-  for index, row in df.iterrows():
-      asignatura = row['Asignatura']
-      nota = row['Nota']
+def process_document(doc_path, finalDoc, df, index):
+    doc = Document(doc_path)
+    copy_paragraphs(doc.paragraphs, finalDoc)
+    copy_tables(doc.tables, finalDoc)
+    finalDoc.add_page_break() # Insertar un salto de página después de cada documento
+    df.at[index, 'Status copy'] = 'OK'
+
+def run_script(targetDir, finalDir, docName, excelDir):
+    finalDoc, docDir, targetDocs = get_folders(targetDir, finalDir, docName)
+    root = Path(targetDocs)
+
+    # Crear un diccionario que especifique el tipo de datos para cada columna
+    column_dtypes = {'Asignatura': str, 'Nota': float, 'Status copy': str}
+
+    df = pd.read_excel(excelDir, sheet_name='Sheet1', header=0, dtype=column_dtypes)
+    for column in df.columns:
+        df[column] = df[column].map(lambda x: x.strip() if isinstance(x, str) else x)
+
+    # Cambiar el tipo de datos de la columna 'Status copy' a cadena de caracteres
+    df['Status copy'] = df['Status copy'].astype(str)
+
+    # Iterar sobre las filas del DataFrame
+    for index, row in df.iterrows():
+        asignatura = row['Asignatura']
+        nota = row['Nota']
+        archivo_encontrado = False
       
-      # Buscar archivos en la carpeta que coincidan con el nombre de asignatura y tengan nota >= 3
-      for doc_path in root.rglob(f'{asignatura}*.docx'):
-          if nota >= 3:
-              # Procesar el documento
-              doc = Document(doc_path)
-              copy_paragraphs(doc.paragraphs, finalDoc)
-              copy_tables(doc.tables, finalDoc)
-              finalDoc.add_page_break() # Insertar un salto de página después de cada documento
+        # Buscar archivos en la carpeta que coincidan con el nombre de asignatura y tengan nota >= 3
+        for doc_path in root.rglob(f'{asignatura}*.docx'):
+            archivo_encontrado = True
+            if nota >= 3:
+                process_document(doc_path, finalDoc, df, index)
+            else:
+                df.at[index, 'Status copy'] = 'N/A'
 
-  # Guardar el documento final
-  finalDoc.save(docDir)
-  
+        if nota >= 3 and not archivo_encontrado:
+            finalDoc.add_paragraph(f'Falta archivo: {asignatura}')
+            df.at[index, 'Status copy'] = 'Falta archivo'
+
+    # Guardar el documento final
+    finalDoc.save(docDir)
+
+    # Guardar el status de la copia en el archivo Excel
+    df.to_excel(excelDir, index=False)
+    adjust_column_widths(excelDir)
+    open_excel(excelDir)
+
+
 targetDir = 'C:/Users/user/Desktop/Trabajos/scriptWord/DocumentosObjetivos'
 finalDir = 'C:/Users/user/Desktop/Trabajos/scriptWord/DocumentoFusionado/'
-docName = 'Ana'
+docName = 'Ana Maria'
 excelDir = 'C:/Users/user/Desktop/Trabajos/scriptWord/Plantilla.xlsx'
-runScript(targetDir, finalDir, docName, excelDir)
 
-  
+#create_template()
+run_script(targetDir, finalDir, docName, excelDir)
